0d274685fdecb76a94af657cdaebedaa083122c0
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-05 21:33:04 +0300

    feat: 45 tests passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -542,8 +542,19 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let result = 0;
+  let count = 0;
+  nums.reduce((acc, value, index) => {
+    if (nums[index] < nums[index + 1]) {
+      count += 1;
+    } else {
+      if (count > result) result = count;
+      count = 0;
+    }
+    return count;
+  }, 0);
+  return result + 1;
 }
 
 /**
