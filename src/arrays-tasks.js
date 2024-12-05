6a0b9dc6f2c162a70fb2490c4473bfe196c092e0
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-05 21:31:30 +0300

    feat: 44 tests passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -520,8 +520,15 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const result = [];
+  arr1.map((value) => {
+    if (arr2.includes(value)) {
+      result.push(value);
+    }
+    return value;
+  });
+  return result;
 }
 
 /**
