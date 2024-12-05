ed57c5afd6a3883f272674c111c6d7336643f578
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-05 21:19:10 +0300

    feat: 36 tests passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -347,8 +347,22 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  let count = 0;
+  const result = arr.reduce(
+    (acc, value) => {
+      if (acc[count].length < chunkSize) {
+        acc[count].push(value);
+      } else {
+        count += 1;
+        acc.push([]);
+        acc[count].push(value);
+      }
+      return acc;
+    },
+    [[]]
+  );
+  return result;
 }
 
 /**
