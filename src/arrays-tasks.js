54dc9de752dfc5d0783809b6e0e0fc5aa78ecb95
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-05 21:27:52 +0300

    feat: 41 tests passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -463,8 +463,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const result = numbers.map((value, index) => {
+    if (value % 2 !== 0) return index;
+    return false;
+  });
+  return result.filter((e) => e !== false);
 }
 
 /**
